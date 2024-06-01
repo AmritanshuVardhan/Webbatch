869986c6eff3678ba788084d6cfe6a33fc7d4d1a
AmritanshuVardhan <168075790+AmritanshuVardhan@users.noreply.github.com>
2024-06-02 00:12:06 +0530

    jsintro

---
+++ Video54/script.js
@@ -0,0 +1,18 @@
+alert("Hello Box Mere Bhai");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a Goat")
+
+var a = prompt("Enter your number")
+var isTrue = prompt("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("computer is blasting")
+}
+
+else{
+    console.log("Computer is not blasting")
+}
+
+console.log("Your number is " +a)
